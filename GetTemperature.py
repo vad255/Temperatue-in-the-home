453dd40b774284af2726b8207343d5cf461c2424
vad255 <vad255@gmail.com>
2017-09-30 12:39:41 +0300

    new try

--- GetTemperature.py
+++ GetTemperature.py
@@ -4,31 +4,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 CREDENTIALS_FILE = r'Temperature in the home-fcc9bfe72949.json'
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',
-                                                                                  'https://www.googleapis.com/auth/drive'])
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, 'https://www.googleapis.com/auth/spreadsheets')
 httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
 
-spreadsheet = service.spreadsheets().create(body = {
-    'properties': {'title': 'Сие есть название документа', 'locale': 'ru_RU'},
-    'sheets': [{'properties': {'sheetType': 'GRID',
-                               'sheetId': 0,
-                               'title': 'TestSheet',
-                               'gridProperties': {'rowCount': 8, 'columnCount': 5}}}]
-}).execute()
+result = service.spreadsheets().values().get(spreadsheetId='1BlPfg3N4POh6Rn3rMcS1k0YDc2YR4tW9Z6C7Qom2o4E', range='Class Data!A2:E').execute()
 
+values = result.get('values', [])
 
-
-results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet['spreadsheetId'], body = {
-    "valueInputOption": "USER_ENTERED",
-    "data": [
-        {"range": "TestSheet!B2:C3",
-         "majorDimension": "ROWS",     # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
-         "values": [["This is B2", "This is C2"], ["This is B3", "This is C3"]]},
-
-        {"range": "TestSheet!D5:E6",
-         "majorDimension": "COLUMNS",  # сначала заполнять столбцы, затем ряды (т.е. самые внутренние списки в values - это столбцы)
-         "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]}
-    ]
-}).execute()
+if not values:
+    print('No data found.')
+else:
+    print('Name, Major:')
+    for row in values:
+        # Print columns A and E, which correspond to indices 0 and 4.
+        print('%s, %s' % (row[0], row[4]))
